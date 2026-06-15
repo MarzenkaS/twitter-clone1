74182df2dac17ee5a36e95e3fc5bb8a42ad46c49
Marzena <maczenta@gmail.com>
2026-06-15 15:06:43 +0200

    change page.jsx in tweet

--- app/tweet/page.jsx
+++ app/tweet/page.jsx
@@ -1,4 +1,8 @@
-export default function Page() {
+import { connectDB } from "@/app/lib/mongoose";
+
+export default async function Page() {
+  await connectDB();
+
   return (
     <div>
       Tweet page
