69296d1b3d1f5226d75af0e03a8d0e62f88a3187
Marzena <maczenta@gmail.com>
2026-06-15 15:45:29 +0200

    feat(tweet): implement tweets index page using TweetCard

--- app/tweet/page.jsx
+++ app/tweet/page.jsx
@@ -1,7 +1,22 @@
-export default function TweetPage() {
+import Link from "next/link";
+import TweetCard from "@/components/TweetCard";
+
+async function getTweets() {
+  const res = await fetch("https://dummyjson.com/posts");
+  return res.json();
+}
+
+export default async function TweetPage() {
+  const tweets = await getTweets();
+
   return (
     <main>
-      <h1>TEST</h1>
+      <h1>📝 Tweets</h1>
+      {tweets.posts.map((tweet) => (
+        <Link key={tweet.id} href={`/tweet/${tweet.id}`}>
+          <TweetCard tweet={tweet} />
+        </Link>
+      ))}
     </main>
   );
 }
